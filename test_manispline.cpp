e36bfe41724190b402153a38203a1ae4bcd52595
O B thuis <olaf.booij@xs4all.nl>
2020-04-11 11:50:55 +0200

    refactored, using Eigen.

--- test_manispline.cpp
+++ test_manispline.cpp
@@ -4,24 +4,33 @@
 #include<iostream>
 #include<random>
 #include<array>
+#include<Eigen/Core>
 #undef NDEBUG
 #include<cassert>
 #define NDEBUG
 
 namespace manispline {
-  auto interpolate(const std::array<auto, 4>& v, const double u1)
+  static Eigen::Matrix<double, 3, 4> cumulative_cubic_B_spline_coefficients()
   {
-    const auto v_cummel_0 = 0.;
+    Eigen::Matrix<double, 3, 4> B;
+    B <<  5,  3, -3,  1,
+          1,  3,  3, -2,
+          0,  0,  0,  1;
+    B /= 6;
+    return B;
+  }
+  Eigen::Vector3d compute_weights(const double delta_time)
+  {
+    Eigen::Vector4d time_power{1, delta_time, delta_time*delta_time, delta_time*delta_time*delta_time};
+    return cumulative_cubic_B_spline_coefficients() * time_power;
+  }
+  double interpolate(const std::array<auto, 4>& v, const double delta_time)
+  {
     const auto v_cummel_1 = v[1] - v[0];
     const auto v_cummel_2 = v[2] - v[1];
     const auto v_cummel_3 = v[3] - v[2];
-    const auto u0 = 1;
-    const auto u2 = u1 * u1;
-    const auto u3 = u2 * u1;
-    return v[0] + (6 * u0 + 0 * u1 + 0 * u2 + 0 * u3) / 6. * v_cummel_0 +
-                  (5 * u0 + 3 * u1 - 3 * u2 + 1 * u3) / 6. * v_cummel_1 +
-                  (1 * u0 + 3 * u1 + 3 * u2 - 2 * u3) / 6. * v_cummel_2 +
-                  (0 * u0 + 0 * u1 + 0 * u2 + 1 * u3) / 6. * v_cummel_3;
+    auto weights = compute_weights(delta_time);
+    return v[0] + weights(0) * v_cummel_1 + weights(1) * v_cummel_2 + weights(2) * v_cummel_3;
   }
 
 };
@@ -38,19 +47,20 @@ struct eu
 int main()
 {
   using namespace manispline;
+  auto close = [](auto a, auto b){return fabs(a - b) < 1e-7;};
   {
-    assert(eu::log(1.2) == 1.2);
-    assert(eu::exp(1.2) == 1.2);
-    assert(eu::prod(1.2, 1.3) == 2.5);
-    assert(eu::prod(1.2, 1.3, 0.3) == 2.8);
+    assert(close(eu::log(1.2), 1.2));
+    assert(close(eu::exp(1.2), 1.2));
+    assert(close(eu::prod(1.2, 1.3), 2.5));
+    assert(close(eu::prod(1.2, 1.3, 0.3), 2.8));
   }
   {
     std::array v{1., 1., 1., 1.};
-    assert(interpolate(v, 1.2) == 1.);
+    assert(close(interpolate(v, 1.2), 1.));
   }
   {
     std::array v{0., 1., 2., 3.};
-    assert(interpolate(v, .3) == 1.3);
+    assert(close(interpolate(v, .3), 1.3));
   }
   {
     std::array v{0., 0., 0., 10.};
@@ -61,7 +71,6 @@ int main()
     assert(interpolate(v, .1) > 0.);
     assert(interpolate(v, .6) > 0.);
   }
-
 
   return 0;
 }
